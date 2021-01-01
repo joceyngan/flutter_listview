ec36747e7476d483efb4537802ccfa246e7abb90
JN <jocey.ngan@gmail.com>
2021-01-02 02:06:11 +0800

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,43 @@
 import 'package:flutter/material.dart';
 
-main(){
-    runApp(MyApp());
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    //throw UnimplementedError();
+    return _MyAppState();
+  }
 }
 
-class MyApp extends StatelessWidget{
-  build(context){
+class _MyAppState extends State{
+  List<String> products = ['Dessert']; 
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('A List'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.all(10),
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text('Add Product'),
+              ),
+            ),
+            Card(
+              child: Column(
+                children: <Widget>[
+                  Image.asset('assets/test_bakeryimages/headimage.jpg'),
+                  Text('Foods')
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
